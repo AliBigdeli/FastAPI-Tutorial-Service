763daf5c0803b89c1557c5e7791bee685fd31375
TheAliBigdeli <bigdeli.ali3@gmail.com>
2025-02-25 00:31:52 +0330

    added celery task

--- core/main.py
+++ core/main.py
@@ -125,6 +125,20 @@ async def initiate_task(background_tasks: BackgroundTasks):
     background_tasks.add_task(start_task,task_id=random.randint(1,100))
     return JSONResponse(content={"detail":"task is done"})
 
+from core.celery_conf import add_number
+from celery.result import AsyncResult
+
+
+@app.get("/initiate-celery-task", status_code=200)
+async def initiate_celery_task():
+    
+    return JSONResponse(content={"detail":add_number.delay(1,2).id})
+
+@app.get("/check-celery-task-result", status_code=200)
+async def initiate_celery_task(task_id:str):
+    result = AsyncResult(task_id).ready()
+    return JSONResponse(content={"result":result})
+
 
 
 
